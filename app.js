01d4c3c32e840508befce55a25746d66dd7fd2db
wissmedia <wissmedia@gmail.com>
2021-07-29 23:07:05 +0800

    Update app.js
    Register Folder Statik

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // -- IMPORT --
 const express = require('express')
+const path = require('path')
 // -- END IMPORT--
 
 // -- EXPRESS APP --
 const app = express()
 const port = process.env.PORT || 2020
 
+// buka port
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
+// register file statik
+app.use(express.static(path.join(__dirname, 'public')))
 // -- END EXPRESS APP --
 
 // -- APP ROUTES --
