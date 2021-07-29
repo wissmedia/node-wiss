fc201a1f96b921f074d2d41305d6e683023a19f8
wissmedia <wissmedia@gmail.com>
2021-07-29 22:58:28 +0800

    Memakai dan Menjalankan Express App

---
+++ app.js
@@ -0,0 +1,17 @@
+// -- IMPORT --
+const express = require('express')
+// -- END IMPORT--
+
+// -- EXPRESS APP --
+const app = express()
+const port = process.env.PORT || 2020
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
+// -- END EXPRESS APP --
+
+// -- APP ROUTES --
+app.get('/', (req, res) => {
+  res.json({message: 'app is running'})
+})
