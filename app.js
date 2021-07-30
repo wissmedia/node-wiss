4ab957953c93497430fe36e707f25d834d609fbe
wissmedia <wissmedia@gmail.com>
2021-07-30 16:29:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+
+const Question = require('./models/question')
 // -- END IMPORT--
 
 // -- EXPRESS APP --
 const app = express()
 const port = process.env.PORT || 2020
+const host = '0.0.0.0'
 const dbURI = 'mongodb://kitakoleksi.my.id:27017/node-wiss'
 
 // MOngoDB Connect dan buka port app
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(result => {
-  console.log(`Connected to DB at ${dbURI}`)
-  // buka port
-  app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    console.log(`Connected to DB at ${dbURI}`)
+    // buka port
+    app.listen(port, host, () => {
+      console.log(`App listening at http://${host}:${port}`)
+    })
   })
-})
-.catch(err => console.log(err))
+  .catch(err => console.log(err))
 
 // register view engine
 app.set('view engine', 'ejs')
@@ -33,8 +36,6 @@ app.use(morgan('dev'))
 // -- END EXPRESS APP --
 
 // -- APP ROUTES --
-
-
 // index
 app.get('/', (req, res) => {
   const navMenus = [
@@ -43,35 +44,59 @@ app.get('/', (req, res) => {
     { link: '/result', icon: 'fas fa-poll', label: 'Hasil' },
     { link: '/settings', icon: 'fas fa-cogs', label: 'Pengaturan' },
   ]
-  res.render('index', {navTitle: 'Beranda', navMenus})
+  res.render('index', { navTitle: 'Beranda', navMenus })
 })
 
 // qbank
 app.get('/qbank', (req, res) => {
   const menus = []
   const navMenus = [
-    { link: '/qbank-add', icon: 'fas fa-plus-circle', label: 'Tambah' },
+    { link: '/', icon: 'fas fa-home', label: 'Beranda' },
+    { link: '/qbank/add', icon: 'fas fa-plus-circle', label: 'Tambah' },
   ]
-  res.render('pages/qbank', {navTitle: 'Bank Pertanyaan',menus, navMenus})
+  Question.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('pages/qbank', { navTitle: 'Bank Pertanyaan', menus, navMenus, questions: result })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 })
-app.get('/qbank-add', (req, res) => {
+app.get('/qbank/add', (req, res) => {
   const menus = []
   const navMenus = []
-  res.render('pages/qbank-add', {navTitle: 'Tambah Pertanyaan', menus, navMenus})
+  res.render('pages/qbank-add', { navTitle: 'Tambah Pertanyaan', menus, navMenus })
 })
+
+// try db, delete later
+// app.get('/qbank-add', (req, res) => {
+//   const question = new Question({
+//     title: 'First Q',
+//     desc: 'Nothing else'
+//   })
+
+//   question.save()
+//     .then((result) => {
+//       res.send(result)
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// })
+// end try db
 
 // kuesioner
 app.get('/quesioner', (req, res) => {
   const menus = []
   const navMenus = []
-  res.render('pages/quesioner', {navTitle: 'Quesioner', menus, navMenus})
+  res.render('pages/quesioner', { navTitle: 'Quesioner', menus, navMenus })
 })
 
 // hasil
 app.get('/result', (req, res) => {
   const menus = []
   const navMenus = []
-  res.render('pages/result', {navTitle: 'Result', menus, navMenus})
+  res.render('pages/result', { navTitle: 'Result', menus, navMenus })
 })
 
 // settings
@@ -80,11 +105,11 @@ app.get('/settings', (req, res) => {
     { link: '/', icon: 'fas fa-user-circle', label: 'Akun' },
   ]
   const navMenus = []
-  res.render('pages/settings', {navTitle: 'Settings', menus, navMenus})
+  res.render('pages/settings', { navTitle: 'Settings', menus, navMenus })
 })
 
 // 404 page
 app.use((req, res) => {
-	res.status(404).render('404', { navTitle: '404' })
+  res.status(404).render('404', { navTitle: '404' })
 })
 // -- END APP ROUTES --
