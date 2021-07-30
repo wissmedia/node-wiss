6a75d8adc450e89f79557665b71c0e8557eb0714
wissmedia <wissmedia@gmail.com>
2021-07-30 21:12:48 +0800

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,28 @@
 // Fungsi autofocus Checkbox Input
-document.getElementById("ck_other").addEventListener('click',function(){
+document.getElementById("ck_other").addEventListener('click', function () {
     let input_other = document.getElementById("input_other");
     input_other.focus();
 });
 
 // Tampilkan menu User
-function tampilMenu(){
+function tampilMenu() {
     let show = document.getElementById("menuakun");
     if (show.style.display === "block") {
         show.style.display = "none";
-    }else{
+    } else {
         show.style.display = "block";
     }
     console.log('sukses');
     return show;
-    
+
 }
 
 // Tampilkan menu shortcut
-function tampilShortcut(){
+function tampilShortcut() {
     let shortcut = document.getElementById("container-shortcut");
     if (shortcut.style.display === "block") {
         shortcut.style.display = "none";
-    }else{
+    } else {
         shortcut.style.display = "block";
     }
     console.log('sukses');
