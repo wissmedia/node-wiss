49ea31e24f68f4a3fb0d825f7c65052f92e795a6
wissmedia <wissmedia@gmail.com>
2021-08-02 08:39:54 +0800

    Update authControllers.js
    Error Handling Auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,3 +1,23 @@
+// signup_get, login_get, signup_post, login_post
+const User = require('../models/user')
+
+const handleErrors = (err) => {
+  console.log(err.message, err.code)
+  let errors = { email: '', password: '' }
+  // duplicate errors
+  if (err.code === 11000) {
+    errors.email = 'Email is already registered'
+    return errors
+  }
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message
+    })
+  }
+  return errors
+}
+
 const signup_get = (req, res) => {
   const menus = []
   const navMenus = []
@@ -8,13 +28,18 @@ const login_get = (req, res) => {
   const navMenus = []
   res.render('login', { navTitle: "Masuk", menus, navMenus, })
 }
-const signup_post = (req, res) => {
+const signup_post = async (req, res) => {
   const { email, password } = req.body
 
-  console.log(email, password)
-  res.send('new signup')
+  try {
+    const user = await User.create({ email, password })
+    res.status(201).json(user)
+  } catch (err) {
+    const errors = handleErrors(err)
+    res.status(400).json({ errors })
+  }
 }
-const login_post = (req, res) => {
+const login_post = async (req, res) => {
   const { email, password } = req.body
 
   console.log(email, password)
