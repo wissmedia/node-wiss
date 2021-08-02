0e59ba1c81f366163a61e935140e36a95f306809
wissmedia <wissmedia@gmail.com>
2021-08-02 08:39:38 +0800

    Create user.js
    Create User Model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const { isEmail } = require('validator')
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter a valid email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  },
+}, { timestamps: true })
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
