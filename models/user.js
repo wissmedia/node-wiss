49351ea679910375696b7496114335c45d714aa4
wissmedia <wissmedia@gmail.com>
2021-08-02 09:09:48 +0800

    Update user.js
    add function to hash password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const { isEmail } = require('validator')
 
 const userSchema = new mongoose.Schema({
@@ -15,6 +16,25 @@ const userSchema = new mongoose.Schema({
     minlength: [6, 'Minimum password length is 6 characters'],
   },
 }, { timestamps: true })
+
+// fire a func before doc saved to db (hash password)
+userSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+// // fire a func after doc saved to db
+// userSchema.post('save', function (doc, next) {
+//   console.log('new user was created & saved', doc)
+//   next()
+// })
+
+// // fire a func before doc saved to db
+// userSchema.pre('save', function (next) {
+//   console.log('user about to be created & saved', this)
+//   next()
+// })
 
 const User = mongoose.model('user', userSchema)
 
