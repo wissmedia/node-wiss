156ebff7f3180db642c509ce889c89f6414b2324
wissmedia <wissmedia@gmail.com>
2021-08-02 18:44:21 +0800

    Update user.js
    add name to user schema

--- models/user.js
+++ models/user.js
@@ -3,6 +3,9 @@ const bcrypt = require('bcrypt')
 const { isEmail } = require('validator')
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
   email: {
     type: String,
     required: [true, 'Please enter a valid email'],
