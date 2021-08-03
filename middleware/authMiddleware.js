861ef3df921686df698fa23fd9eb8347ad44ca99
wissmedia <wissmedia@gmail.com>
2021-08-03 11:56:44 +0800

    Update authMiddleware.js
    create check user middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt
@@ -19,6 +20,29 @@ const requireAuth = (req, res, next) => {
   }
 }
 
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt
+  if (token) {
+    jwt.verify(token, 'node wiss secret', async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message)
+        res.locals.user = null
+        next()
+      } else {
+        console.log(decodedToken)
+        let user = await User.findById(decodedToken.id)
+        res.locals.user = user
+        next()
+      }
+    })
+  } else {
+    res.locals.user = null
+    next()
+  }
+}
+
 module.exports = {
-  requireAuth
+  requireAuth,
+  checkUser,
 }
